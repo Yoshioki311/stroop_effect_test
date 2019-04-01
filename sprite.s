da74635827cd8f34e1e0249b12e4b6f16889e985
karen-poon <karen-poon@users.noreply.github.com>
2019-03-31 21:44:49 -0400

    updated sprite.s file

--- sprite.s
+++ sprite.s
@@ -1,4 +1,39 @@
-                .global TITLEPAGE
-TITLEPAGE:      .incbin "title.bin"
-                .global RED_CHOICE
-RED_CHOICE:     .incbin "red_choice.bin"
+                    .global TITLEPAGE
+TITLEPAGE:          .incbin "title.bin"
+
+                    .global BLUE_CHOICE
+BLUE_CHOICE:        .incbin "blue_choice.bin"
+                    .global GREEN_CHOICE
+GREEN_CHOICE:       .incbin "green_choice.bin"
+                    .global RED_CHOICE
+RED_CHOICE:         .incbin "red_choice.bin"
+                    .global YELLOW_CHOICE
+YELLOW_CHOICE:      .incbin "yellow_choice.bin"
+
+                    .global BLUE_IN_GREEN
+BLUE_IN_GREEN:      .incbin "blue_in_green.bin"
+                    .global BLUE_IN_RED
+BLUE_IN_RED:        .incbin "blue_in_red.bin"
+                    .global BLUE_IN_YELLOW
+BLUE_IN_YELLOW:     .incbin "blue_in_yellow.bin"
+
+                    .global GREEN_IN_BLUE
+GREEN_IN_BLUE:      .incbin "green_in_blue.bin"
+                    .global GREEN_IN_RED
+GREEN_IN_RED:       .incbin "green_in_red.bin"
+                    .global GREEN_IN_YELLOW
+GREEN_IN_YELLOW:    .incbin "green_in_yellow.bin"
+
+                    .global RED_IN_BLUE
+RED_IN_BLUE:        .incbin "red_in_blue.bin"
+                    .global RED_IN_GREEN
+RED_IN_GREEN:       .incbin "red_in_green.bin"
+                    .global RED_IN_YELLOW
+RED_IN_YELLOW:      .incbin "red_in_yellow.bin"
+
+                    .global YELLOW_IN_BLUE
+YELLOW_IN_BLUE:     .incbin "yellow_in_blue.bin"
+                    .global YELLOW_IN_GREEN
+YELLOW_IN_GREEN:    .incbin "yellow_in_green.bin"                
+                    .global YELLOW_IN_RED
+YELLOW_IN_RED:      .incbin "yellow_in_red.bin"
