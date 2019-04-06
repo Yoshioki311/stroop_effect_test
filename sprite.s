d6f8cad305de49400270dd4a09063c9a47699c56
Sophie Mao <shsm715@gmail.com>
2019-04-06 18:48:52 -0400

    Modified bin file access directory address

--- sprite.s
+++ sprite.s
@@ -1,39 +1,39 @@
                     .global TITLEPAGE
-TITLEPAGE:          .incbin "title.bin"
+TITLEPAGE:          .incbin "/bin_files/title.bin"
 
                     .global BLUE_CHOICE
-BLUE_CHOICE:        .incbin "blue_choice.bin"
+BLUE_CHOICE:        .incbin "/bin_files/blue_choice.bin"
                     .global GREEN_CHOICE
-GREEN_CHOICE:       .incbin "green_choice.bin"
+GREEN_CHOICE:       .incbin "/bin_files/green_choice.bin"
                     .global RED_CHOICE
-RED_CHOICE:         .incbin "red_choice.bin"
+RED_CHOICE:         .incbin "/bin_files/red_choice.bin"
                     .global YELLOW_CHOICE
-YELLOW_CHOICE:      .incbin "yellow_choice.bin"
+YELLOW_CHOICE:      .incbin "/bin_files/yellow_choice.bin"
 
                     .global BLUE_IN_GREEN
-BLUE_IN_GREEN:      .incbin "blue_in_green.bin"
+BLUE_IN_GREEN:      .incbin "/bin_files/blue_in_green.bin"
                     .global BLUE_IN_RED
-BLUE_IN_RED:        .incbin "blue_in_red.bin"
+BLUE_IN_RED:        .incbin "/bin_files/blue_in_red.bin"
                     .global BLUE_IN_YELLOW
-BLUE_IN_YELLOW:     .incbin "blue_in_yellow.bin"
+BLUE_IN_YELLOW:     .incbin "/bin_files/blue_in_yellow.bin"
 
                     .global GREEN_IN_BLUE
-GREEN_IN_BLUE:      .incbin "green_in_blue.bin"
+GREEN_IN_BLUE:      .incbin "/bin_files/green_in_blue.bin"
                     .global GREEN_IN_RED
-GREEN_IN_RED:       .incbin "green_in_red.bin"
+GREEN_IN_RED:       .incbin "/bin_files/green_in_red.bin"
                     .global GREEN_IN_YELLOW
-GREEN_IN_YELLOW:    .incbin "green_in_yellow.bin"
+GREEN_IN_YELLOW:    .incbin "/bin_files/green_in_yellow.bin"
 
                     .global RED_IN_BLUE
-RED_IN_BLUE:        .incbin "red_in_blue.bin"
+RED_IN_BLUE:        .incbin "/bin_files/red_in_blue.bin"
                     .global RED_IN_GREEN
-RED_IN_GREEN:       .incbin "red_in_green.bin"
+RED_IN_GREEN:       .incbin "/bin_files/red_in_green.bin"
                     .global RED_IN_YELLOW
-RED_IN_YELLOW:      .incbin "red_in_yellow.bin"
+RED_IN_YELLOW:      .incbin "/bin_files/red_in_yellow.bin"
 
                     .global YELLOW_IN_BLUE
-YELLOW_IN_BLUE:     .incbin "yellow_in_blue.bin"
+YELLOW_IN_BLUE:     .incbin "/bin_files/yellow_in_blue.bin"
                     .global YELLOW_IN_GREEN
-YELLOW_IN_GREEN:    .incbin "yellow_in_green.bin"                
+YELLOW_IN_GREEN:    .incbin "/bin_files/yellow_in_green.bin"                
                     .global YELLOW_IN_RED
-YELLOW_IN_RED:      .incbin "yellow_in_red.bin"
+YELLOW_IN_RED:      .incbin "/bin_files/yellow_in_red.bin"
