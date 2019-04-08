9cc313f249ac71ccfd8791edde24104ad8b31555
Sophie Mao <shsm715@gmail.com>
2019-04-08 17:09:28 -0400

    Updated sprites and functions for vga display

--- vga_display.c
+++ vga_display.c
@@ -1,243 +1,213 @@
-extern volatile int pixel_buffer_start;
-extern short TITLEPAGE [240][320];
+extern volatile int pixel_buffer_start; 
+extern short title[76800];
 
-extern short BLUE_CHOICE [40][60];
-extern short GREEN_CHOICE [40][60];
-extern short RED_CHOICE [40][60];
-extern short YELLOW_CHOICE [40][60];
+extern short blue_choice[2400];
+extern short green_choice[2400];
+extern short red_choice[2400];
+extern short yellow_choice[2400];
 
-extern short BLUE_IN_GREEN [90][160];
-extern short BLUE_IN_RED [90][160];
-extern short BLUE_IN_YELLOW [90][160];
+extern short blue_in_green[14400];
+extern short blue_in_red[14400];
+extern short blue_in_yellow[14400];
+extern short green_in_blue[14400];
+extern short green_in_red[14400];
+extern short green_in_yellow[14400];
+extern short red_in_blue[14400];
+extern short red_in_green[14400];
+extern short red_in_yellow[14400];
+extern short yellow_in_blue[14400];
+extern short yellow_in_red[14400];
+extern short yellow_in_green[14400];
 
-extern short GREEN_IN_BLUE [90][160];
-extern short GREEN_IN_RED [90][160];
-extern short GREEN_IN_YELLOW [90][160];
+/*----------------Function declaration----------------*/ 
+void plot_pixel(int x, int y, short int line_colour);
+void clear_screen ();
+void draw_title_page ();
 
-extern short RED_IN_BLUE [90][160];
-extern short RED_IN_GREEN [90][160];
-extern short RED_IN_YELLOW [90][160];
+void draw_blue_choice(int x);
+void draw_green_choice(int x);
+void draw_red_choice(int x);
+void draw_yellow_choice(int x);
+void draw_blue_in_green ();
+void draw_blue_in_red ();
+void draw_blue_in_yellow ();
+void draw_green_in_blue ();
+void draw_green_in_red ();
+void draw_green_in_yellow ();
+void draw_yellow_in_blue ();
+void draw_yellow_in_red ();
+void draw_yellow_in_green ();
+/*----------------------------------------------------*/
 
-extern short YELLOW_IN_BLUE [90][160];
-extern short YELLOW_IN_GREEN [90][160];
-extern short YELLOW_IN_RED [90][160];
-
-void clear_screen() {
-    for(int x = 0; x < 320; ++x) {
-        for(int y = 0; y < 240; ++y) {
-            *(short int *)(pixel_buffer_start + (y << 10) + (x << 1)) = 0;
-        }
-    }
+void plot_pixel(int x, int y, short int line_colour) {
+	*(short int *) (pixel_buffer_start + (y<<10)+(x<<1)) = line_colour;
 }
 
-void draw_title_page () {
-    int i = 0;
-    int j = 0;
-    for (i = 0; i < 240; i++)
-        for (j = 0; j < 320; j++)
-            *(short int *)(pixel_buffer_start + (i << 10) + (j << 1)) = TITLEPAGE[i][j];
-}
+void clear_screen() { 
+	int x, y;
+    for(x = 0; x < 320; ++x) { 
+        for(y = 0; y < 240; ++y) { 
+            *(short int *)(pixel_buffer_start + (y << 10) + (x << 1)) = 0; 
+        } 
+    } 
+} 
 
-void draw_word (int rand_index) {
-    switch (rand_index) {
-        case 0: draw_blue_in_green ();
-                break;
-        case 1: draw_green_in_blue ();
-                break;
-        case 2: draw_red_in_blue ();
-                break;
-        case 3: draw_yellow_in_blue ();
-                break;
-        case 4: draw_blue_in_red ();
-                break;
-        case 5: draw_green_in_red ();
-                break;
-        case 6: draw_red_in_green ();
-                break;
-        case 7: draw_yellow_in_green ();
-                break;
-        case 8: draw_blue_in_yellow ();
-                break;
-        case 9: draw_green_in_yellow ();
-                break;
-        case 10: draw_red_in_yellow ();
-                 break;
-        case 11: draw_yellow_in_red ();
-                 break;
-    }
-}
-
-void draw_choice (int * choice) {
-    switch(choice[0]) {
-        case 0: draw_blue_choice(34);
-                break;
-        case 1: draw_green_choice(34);
-                break;
-        case 2: draw_red_choice(34);
-                break;
-        case 3: draw_yellow_choice(34);
-                break;
-    }
-    switch(choice[1]) {
-        case 0: draw_blue_choice(108);
-                break;
-        case 1: draw_green_choice(108);
-                break;
-        case 2: draw_red_choice(108);
-                break;
-        case 3: draw_yellow_choice(108);
-                break;
-    }
-    switch(choice[2]) {
-        case 0: draw_blue_choice(172);
-                break;
-        case 1: draw_green_choice(172);
-                break;
-        case 2: draw_red_choice(172);
-                break;
-        case 3: draw_yellow_choice(172);
-                break;
-    }
-    switch(choice[3]) {
-        case 0: draw_blue_choice(236);
-                break;
-        case 1: draw_green_choice(236);
-                break;
-        case 2: draw_red_choice(236);
-                break;
-        case 3: draw_yellow_choice(236);
-                break;
-    }
+void draw_title_page() {
+	int i, j;
+	for (j = 0; j < 240; ++j) {
+		for (i = 0; i < 320; ++i) {
+			plot_pixel(i, j, title[j*320+i]);
+		}
+	}
 }
 
 /*----------------------------- Choices --------------------------------*/
-void draw_blue_choice (int x) {
-    int i = 0;
-    int j = 0;
-    for (i = 110; i < 150; i++)
-        for (j = x; j < x + 60; j++)
-            *(short int *)(pixel_buffer_start + (i << 10) + (j << 1)) = BLUE_CHOICE[i][j];
+void draw_blue_choice(int x) {
+	int i, j;
+	for (j = 110; j < 150; ++j) {
+		for (i = x; i < x + 60; ++i) {
+			plot_pixel(i, j, blue_choice[(j-110)*60+(i-x)]);
+		}
+	}
 }
 
-void draw_green_choice (int x) {
-    int i = 0;
-    int j = 0;
-    for (i = 110; i < 150; i++)
-        for (j = x; j < x + 60; j++)
-            *(short int *)(pixel_buffer_start + (i << 10) + (j << 1)) = GREEN_CHOICE[i][j];
+void draw_green_choice(int x) {
+	int i, j;
+	for (j = 110; j < 150; ++j) {
+		for (i = x; i < x + 60; ++i) {
+			plot_pixel(i, j, green_choice[(j-110)*60+(i-x)]);
+		}
+	}
 }
 
-void draw_red_choice (int x) {
-    int i = 0;
-    int j = 0;
-    for (i = 110; i < 150; i++)
-        for (j = x; j < x + 60; j++)
-            *(short int *)(pixel_buffer_start + (i << 10) + (j << 1)) = RED_CHOICE[i][j];
+void draw_red_choice(int x) {
+	int i, j;
+	for (j = 110; j < 150; ++j) {
+		for (i = x; i < x + 60; ++i) {
+			plot_pixel(i, j, red_choice[(j-110)*60+(i-x)]);
+		}
+	}
 }
 
-void draw_yellow_choice (int x) {
-    int i = 0;
-    int j = 0;
-    for (i = 110; i < 150; i++)
-        for (j = x; j < x + 60; j++)
-            *(short int *)(pixel_buffer_start + (i << 10) + (j << 1)) = YELLOW_CHOICE[i][j];
+void draw_yellow_choice(int x) {
+	int i, j;
+	for (j = 110; j < 150; ++j) {
+		for (i = x; i < x + 60; ++i) {
+			plot_pixel(i, j, yellow_choice[(j-110)*60+(i-x)]);
+		}
+	}
 }
-/*----------------------------------------------------------------------*/
 
-/*---------------------------- Display word ----------------------------*/
-void draw_blue_in_green () {
-    int i = 0;
-    int j = 0;
-    for (i = 10; i < 100; i++)
-        for (j = 80; j < 240; j++)
-            *(short int *)(pixel_buffer_start + (i << 10) + (j << 1)) = BLUE_IN_GREEN[i][j];
+/*----------------------------------------------------------------------*/ 
+ 
+ 
+/*---------------------------- Display word ----------------------------*/ 
+void draw_blue_in_green() {
+	int i, j;
+	for (j = 10; j < 100; ++j) {
+		for (i = 80; i < 240; ++i) {
+			plot_pixel(i, j, blue_in_green[(j-10)*160+(i-80)]);
+		}
+	}
 }
 
 void draw_blue_in_red () {
-    int i = 0;
-    int j = 0;
-    for (i = 10; i < 100; i++)
-        for (j = 80; j < 240; j++)
-            *(short int *)(pixel_buffer_start + (i << 10) + (j << 1)) = BLUE_IN_RED[i][j];
+	int i, j;
+	for (j = 10; j < 100; ++j) {
+		for (i = 80; i < 240; ++i) {
+			plot_pixel(i, j, blue_in_red[(j-10)*160+(i-80)]);
+		}
+	}
 }
 
 void draw_blue_in_yellow () {
-    int i = 0;
-    int j = 0;
-    for (i = 10; i < 100; i++)
-        for (j = 80; j < 240; j++)
-            *(short int *)(pixel_buffer_start + (i << 10) + (j << 1)) = BLUE_IN_YELLOW[i][j];
+	int i, j;
+	for (j = 10; j < 100; ++j) {
+		for (i = 80; i < 240; ++i) {
+			plot_pixel(i, j, blue_in_yellow[(j-10)*160+(i-80)]);
+		}
+	}
 }
 
 void draw_green_in_blue () {
-    int i = 0;
-    int j = 0;
-    for (i = 10; i < 100; i++)
-        for (j = 80; j < 240; j++)
-            *(short int *)(pixel_buffer_start + (i << 10) + (j << 1)) = GREEN_IN_BLUE[i][j];
+	int i, j;
+	for (j = 10; j < 100; ++j) {
+		for (i = 80; i < 240; ++i) {
+			plot_pixel(i, j, green_in_blue[(j-10)*160+(i-80)]);
+		}
+	}
 }
 
 void draw_green_in_red () {
-    int i = 0;
-    int j = 0;
-    for (i = 10; i < 100; i++)
-        for (j = 80; j < 240; j++)
-            *(short int *)(pixel_buffer_start + (i << 10) + (j << 1)) = GREEN_IN_RED[i][j];
+	int i, j;
+	for (j = 10; j < 100; ++j) {
+		for (i = 80; i < 240; ++i) {
+			plot_pixel(i, j, green_in_red[(j-10)*160+(i-80)]);
+		}
+	}
 }
 
 void draw_green_in_yellow () {
-    int i = 0;
-    int j = 0;
-    for (i = 10; i < 100; i++)
-        for (j = 80; j < 240; j++)
-            *(short int *)(pixel_buffer_start + (i << 10) + (j << 1)) = GREEN_IN_YELLOW[i][j];
+	int i, j;
+	for (j = 10; j < 100; ++j) {
+		for (i = 80; i < 240; ++i) {
+			plot_pixel(i, j, green_in_yellow[(j-10)*160+(i-80)]);
+		}
+	}
 }
 
 void draw_red_in_blue () {
-    int i = 0;
-    int j = 0;
-   for (i = 10; i < 100; i++)
-        for (j = 80; j < 240; j++)
-            *(short int *)(pixel_buffer_start + (i << 10) + (j << 1)) = RED_IN_BLUE[i][j];
+	int i, j;
+	for (j = 10; j < 100; ++j) {
+		for (i = 80; i < 240; ++i) {
+			plot_pixel(i, j, red_in_blue[(j-10)*160+(i-80)]);
+		}
+	}
 }
 
 void draw_red_in_green () {
-    int i = 0;
-    int j = 0;
-    for (i = 10; i < 100; i++)
-        for (j = 80; j < 240; j++)
-            *(short int *)(pixel_buffer_start + (i << 10) + (j << 1)) = RED_IN_GREEN[i][j];
+	int i, j;
+	for (j = 10; j < 100; ++j) {
+		for (i = 80; i < 240; ++i) {
+			plot_pixel(i, j, red_in_green[(j-10)*160+(i-80)]);
+		}
+	}
 }
 
 void draw_red_in_yellow () {
-    int i = 0;
-    int j = 0;
-    for (i = 10; i < 100; i++)
-        for (j = 80; j < 240; j++)
-            *(short int *)(pixel_buffer_start + (i << 10) + (j << 1)) = RED_IN_YELLOW[i][j];
+	int i, j;
+	for (j = 10; j < 100; ++j) {
+		for (i = 80; i < 240; ++i) {
+			plot_pixel(i, j, red_in_yellow[(j-10)*160+(i-80)]);
+		}
+	}
 }
 
 void draw_yellow_in_blue () {
-    int i = 0;
-    int j = 0;
-    for (i = 10; i < 100; i++)
-        for (j = 80; j < 240; j++)
-            *(short int *)(pixel_buffer_start + (i << 10) + (j << 1)) = YELLOW_IN_BLUE[i][j];
-}
-
-void draw_yellow_in_green () {
-    int i = 0;
-    int j = 0;
-    for (i = 10; i < 100; i++)
-        for (j = 80; j < 240; j++)
-            *(short int *)(pixel_buffer_start + (i << 10) + (j << 1)) = YELLOW_IN_GREEN[i][j];
+	int i, j;
+	for (j = 10; j < 100; ++j) {
+		for (i = 80; i < 240; ++i) {
+			plot_pixel(i, j, yellow_in_blue[(j-10)*160+(i-80)]);
+		}
+	}
 }
 
 void draw_yellow_in_red () {
-    int i = 0;
-    int j = 0;
-   for (i = 10; i < 100; i++)
-        for (j = 80; j < 240; j++)
-            *(short int *)(pixel_buffer_start + (i << 10) + (j << 1)) = YELLOW_IN_RED[i][j];
+	int i, j;
+	for (j = 10; j < 100; ++j) {
+		for (i = 80; i < 240; ++i) {
+			plot_pixel(i, j, yellow_in_red[(j-10)*160+(i-80)]);
+		}
+	}
 }
+
+void draw_yellow_in_green () {
+	int i, j;
+	for (j = 10; j < 100; ++j) {
+		for (i = 80; i < 240; ++i) {
+			plot_pixel(i, j, yellow_in_green[(j-10)*160+(i-80)]);
+		}
+	}
+}
+
 /*----------------------------------------------------------------------*/
