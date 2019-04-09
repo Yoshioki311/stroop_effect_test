ca542b7608886468256adc306646dfd57782ff91
Sophie Mao <shsm715@gmail.com>
2019-04-08 23:07:56 -0400

    Cleared comments not needed

--- interrupt_service_routine.s
+++ interrupt_service_routine.s
@@ -86,27 +86,3 @@ PATTERN:        .byte  0x3F					//'0'
                 .skip  2				  	//maintain word alignment
 
                 .end
-
-@ interval_timer_ISR:
-@                 LDR      R0, =0xFF202000 // base address of the interval timer
-@                 MOV      R1, #0x9
-@                 STR      R1, [R0, #4]    // stop the timer
-
-@                 MOVW     R1, #0x5E00     // lower 16 bits of count value
-@                 STR      R1, [R0, #8]    // 100MHz * 30s = 3,000,000,000 = 0xB2D05E00
-@                 MOVW     R1, #0xB2D0     // higher 16 bits of count value
-@                 STR      R1, [R0, #12]   // reload the timer
-
-@                 LDR      R1, [R0]        // read the status register
-@                 AND      R1, #0xFFFFFFFE
-@                 STR      R1, [R0]        // write 0 to TO to reset
-
-@                 LDR      R0, =state
-@                 LDR      R1, [R0]
-@                 CMP      R1, #1
-@                 BNE      END
-
-@                 MOV      R1, #0
-@                 STR      R1, [R0]
-
-@ END:            MOV      PC, LR
